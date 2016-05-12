b8c6add730948b975ff3a28296b4e742b0ed5ba8
C Nicholas Mao <mao@tacktechnologies.com>
2016-05-11 18:31:17 -0700

    Played around with the options

--- treemap/main.js
+++ treemap/main.js
@@ -7,8 +7,9 @@ function main() {
     d3.csv('prepped_data.csv', function(error, data) {
         //console.log(data);
         var chart = TreemapChart();
-        chart.width(500)
-        chart.height(500)
+        chart.width(1000)
+        chart.height(1000)
+        chart.padding(5)
         chart.nestedElement("region").measure("fertility_rate").elementText("country_code")
         var div = d3.select("#vis").datum(data).call(chart);
     })
